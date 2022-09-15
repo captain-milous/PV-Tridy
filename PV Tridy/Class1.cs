ee59c1d736c237e72b427c421292d16c0809741f
captain-milous <milda.tesar3@gmail.com>
2022-09-15 20:31:41 +0200

    New Classes

---
+++ PV Tridy/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PV_Tridy
+{
+    internal class Interval
+    {
+        private int min;
+        private int max;
+
+        public Interval(int min, int max)
+        {
+            if (min <= max)
+            {
+                this.min = min;
+                this.max = max;
+            }
+            else
+            {
+                this.min = max;
+                this.max = min;
+            }
+            
+        }
+        public int Min { 
+            get { return min; } 
+            set { 
+                if(value < max) { 
+                    min = value;
+                }
+            }
+        }
+
+        public int Max { 
+            get { return max; }
+            set
+            {
+                if (value > min)
+                {
+                    max = value;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Interval od "+this.min+" do "+this.max;
+        }
+
+        public bool JeVIntervalu(int cislo, Interval interval)
+        {
+            if(cislo > interval.min && cislo<interval.max)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public Interval ZjistiPrunik(Interval interval1, Interval interval2)
+        {
+            Interval zjisti = null;
+            if(interval1.max >= interval2.min || interval2.max >= interval1.min)
+            {
+                if(interval1.max >= interval2.min)
+                {
+                    zjisti = new Interval(interval2.min, interval1.max);
+                }
+                if (interval2.max >= interval1.min)
+                {
+                    zjisti = new Interval(interval1.min, interval2.max);
+                }
+
+                return zjisti;
+            } else
+            {
+                return null;
+            }
+        }
+    }
+}
